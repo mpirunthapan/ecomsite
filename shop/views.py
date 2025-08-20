15ffc55b17642b06191d81ef25663056233adb33
mpirunthapan <mpirunthapan@gmail.com>
2025-08-20 15:30:36 +0530

    search pagination

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 
 def index(request):
     products = Product.objects.all()
+
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        products = products.filter(name__icontains=item_name)
+
+    paginator = Paginator(products, 12) 
+    page_number = request.GET.get('page')
+    products = paginator.get_page(page_number)
+
     return render(request, 'shop/index.html', {'products': products})
+
