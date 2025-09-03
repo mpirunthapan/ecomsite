d47ddc60c7c24293141624d34210d69aa63aad1a
mpirunthapan <mpirunthapan@gmail.com>
2025-09-03 15:27:52 +0530

    checkout commit

--- shop/models.py
+++ shop/models.py
@@ -10,3 +10,16 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Order(models.Model):
+    items = models.CharField(max_length=3000)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    zip = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.first_name} {self.last_name}"
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Order
 from django.core.paginator import Paginator
 
 def index(request):
@@ -19,4 +19,19 @@ def detail(request, id):
     product = Product.objects.get(id=id)
 
     return render(request, 'shop/detail.html', {'product': product})
+
+def checkout(request):
+
+    if request.method == "POST":
+        firstName = request.POST.get('firstName', '')
+        lastName = request.POST.get('lastName', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address', '')
+        city = request.POST.get('city', '')
+        zip = request.POST.get('zip', '')
+        
+        order = Order(items="", first_name=firstName, last_name=lastName, email=email, address=address, city=city, zip=zip)
+        order.save()
+
+    return render(request, 'shop/checkout.html')
 
